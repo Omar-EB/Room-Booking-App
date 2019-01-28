f46d33a61ff7b376d3e5ba3e37e37429d5923810
Omar-EB <omar.elboraey99@gmail.com>
2019-01-27 22:06:17 -0500

    tests

--- DB_Project/src/main/java/com/team/application/ApplicationController.java
+++ DB_Project/src/main/java/com/team/application/ApplicationController.java
@@ -3,6 +3,7 @@ package com.team.application;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,13 @@ public class ApplicationController {
     @RequestMapping("/")
     public String index() {
         return "index.html";
+    }
+    
+    
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 	
 }
