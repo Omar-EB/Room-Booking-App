050f969dec09ddebba70158af4c8b0e986bebfd7
Omar-EB <omar.elboraey99@gmail.com>
2019-03-02 01:39:35 -0500

    more query tests

--- Room-Booking-App_Proj/src/main/java/com/team/application/RestfulController.java
+++ Room-Booking-App_Proj/src/main/java/com/team/application/RestfulController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,9 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 public class RestfulController {
 
+	@Autowired
+	private UnitService unitService;
+	
 	@RequestMapping("/units")
 	public List<Unit> getUnits(){
-		Unit[] array = new Unit[] {new Unit(1,"Unit-1",1.0), new Unit(2,"Unit-2",2.0), new Unit(3,"Unit-3",3.0)};
-		return Arrays.asList(array);
+		return unitService.getAllUnits();
 	}
+	
 }
---
+++ Room-Booking-App_Proj/src/main/java/com/team/application/UnitService.java
@@ -0,0 +1,20 @@
+package com.team.application;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnitService {
+
+	private List<Unit> units = Arrays.asList(new Unit[] {
+			new Unit(1,"Unit-1",1.0),
+			new Unit(2,"Unit-2",2.0),
+			new Unit(3,"Unit-3",3.0)
+			});
+	
+	public List<Unit> getAllUnits(){
+		return units;
+	}
+}
