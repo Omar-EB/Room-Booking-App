a622c584e4f68f291c608d265479cbf4b79bf9f1
Omar-EB <omar.elboraey99@gmail.com>
2019-03-02 19:52:41 -0500

    insert request fix

--- Room-Booking-App_Proj/src/main/java/com/team/application/SubmissionController.java
+++ Room-Booking-App_Proj/src/main/java/com/team/application/SubmissionController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@SessionAttributes(types = Unit.class)
 @Controller
 public class SubmissionController {
 
@@ -34,8 +33,7 @@ public class SubmissionController {
 	}
 	
 	@PostMapping("/unit")
-	public String getUnit( Unit unit, SessionStatus status){
-		status.setComplete();
+	public String getUnit( @ModelAttribute Unit unit){
 		unitService.addUnit(unit);
 		return "result2";
 	}
