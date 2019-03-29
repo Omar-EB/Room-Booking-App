ad265a9671380a652b9d989a59cbab00b825e0e8
Omar-EB <omar.elboraey99@gmail.com>
2019-03-28 23:11:04 -0400

    hotel model

---
+++ Room-Booking-App_Proj/src/main/java/com/team/application/models/Hotel.java
@@ -0,0 +1,92 @@
+package com.team.application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Hotel")
+public class Hotel {
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hc_name", nullable = false)
+	private HotelChain hotelChain;
+	
+	@Id
+	private int hotel_id;
+	
+	private String street_name;
+    private int street_number;
+    private String city ;
+    private String state ;
+    private String country ;
+    private int rating ;
+    private String phone_number ;
+    private int  number_of_rooms ;
+
+	
+    public HotelChain getHotelChain() {
+		return hotelChain;
+	}
+	public void setHotelChain(HotelChain hotelChain) {
+		this.hotelChain = hotelChain;
+	}
+	public int getHotel_id() {
+		return hotel_id;
+	}
+	public void setHotel_id(int hotel_id) {
+		this.hotel_id = hotel_id;
+	}
+	public String getStreet_name() {
+		return street_name;
+	}
+	public void setStreet_name(String street_name) {
+		this.street_name = street_name;
+	}
+	public int getStreet_number() {
+		return street_number;
+	}
+	public void setStreet_number(int street_number) {
+		this.street_number = street_number;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	public int getNumber_of_rooms() {
+		return number_of_rooms;
+	}
+	public void setNumber_of_rooms(int number_of_rooms) {
+		this.number_of_rooms = number_of_rooms;
+	}
+
+}
--- Room-Booking-App_Proj/src/main/java/com/team/application/models/HotelChain.java
+++ Room-Booking-App_Proj/src/main/java/com/team/application/models/HotelChain.java
@@ -2,8 +2,10 @@ package com.team.application.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "HotelChain")
 public class HotelChain {
 	
 	@Id
