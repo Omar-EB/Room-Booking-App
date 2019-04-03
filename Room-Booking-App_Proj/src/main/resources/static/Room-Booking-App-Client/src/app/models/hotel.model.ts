35e93e9c9e17e0c5a0dd2196276cf8e336fcf5f2
Quang-Vinh Do <qvinh@live.ca>
2019-04-03 02:52:03 -0400

    More work on customer booking

--- Room-Booking-App_Proj/src/main/resources/static/Room-Booking-App-Client/src/app/models/hotel.model.ts
+++ Room-Booking-App_Proj/src/main/resources/static/Room-Booking-App-Client/src/app/models/hotel.model.ts
@@ -1,12 +1,18 @@
+import { HotelChain } from './hotelchain.model';
+
 export class Hotel {
-    hc_name: string;
-    hotel_id: number;
-    street_name: string;
-    street_number: number;
+    hotelChain: HotelChain;
+    hotelId: number;
+    streetName: string;
+    streetNumber: number;
     city: string;
     state: string;
     country: string;
     rating: number;
-    phone_number: string;
-    number_of_rooms: number;
+    phoneNumber: string;
+    numberOfRooms: number;
+
+    public getAddress(): string {
+        return this.streetNumber + ' ' + this.streetName + ' ' + this.city + ', ' + this.state + ', ' + this.country;
+    }
 }
